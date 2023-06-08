e2ad47cb1d45ca34645b25e77254299b7336052a
Uğur Yiğit <101671686+iamuguryigit@users.noreply.github.com>
2023-06-08 16:57:03 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
-retention_data_path = "/Users/uguryigit/PycharmProjects/AB_Testing/datasets/cookie_cats.csv"
+retention_data_path = "../datasets/cookie_cats.csv"
 
-revenue_data_path = "/Users/uguryigit/PycharmProjects/AB_Testing/datasets/AB_Test_Results.csv"
+revenue_data_path = "../datasets/AB_Test_Results.csv"
